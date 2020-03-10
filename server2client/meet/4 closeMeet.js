07be824c5e7501b819d69a2bb093f5bcac3cc41d
randomyige <randomyige@gmail.com>
2020-03-10 14:08:35 +0800

    closeMeet

---
+++ server2client/meet/4 closeMeet.js
@@ -0,0 +1,24 @@
+let msg = {
+    header:{
+        version:'1.0',
+        mid:'',//uuid 消息id
+        uid:'',//发送者id
+        did:'',//发送者设备id
+        action:'closeMeet',
+        time:1234567
+    },
+    body:{
+        meetId:'uuid'
+    }
+}
+
+
+let ack = {
+    header:{
+        version:'1.0',
+        action:'ack'
+    },
+    body:{
+        mid:''
+    }
+}
