28c94dc9a92ec496eb211b531dec06f16fde6fce
randomyige <randomyige@gmail.com>
2020-05-26 16:53:14 +0800

    tmp

---
+++ client2server/msg/5 fetchHistoryMsg.js
@@ -0,0 +1,31 @@
+let msg = {//视频、语音、文件 点击浏览
+    header:{
+        version:'1.0',
+        mid:'',//uuid 消息id
+        action:'playReport'
+    },
+    body:{
+        // chats:[{
+        //     chatId:'',
+        //     targets:[{
+        //         uid:'',
+        //         msgIds:[]
+        //     }]
+        // }]
+        chatId:'',
+        mid:'',
+        targetUid:''
+    }
+}
+let ack = {
+    header:{
+        version:'1.0',
+        action:'ack',
+        time:1234567
+    },
+    body:{
+        mid:'',
+        status:0,
+        err:'',
+    }
+}
